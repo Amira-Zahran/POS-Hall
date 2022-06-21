1af23542a230a52761079c63d9a7567902189900
Amira Zahran <67193358+Amira-Zahran@users.noreply.github.com>
2022-06-21 03:11:17 +0200

    This is a new commit for what I originally planned to be amended

--- lib/final_hall.dart
+++ lib/final_hall.dart
@@ -92,7 +92,9 @@ class _FHallState extends State<FHall> {
                                                           //e.type!.contains('TakeAway') ? Icon(Icons.fastfood, color: Color.fromRGBO(14, 35, 46, 1),) : Icon(Icons.baby_changing_station),
                                                           Center(
                                                             child: Text(
-                                                              'TakeAway'' ${e.orderId}',
+                                                              e.type == 'Floor'
+                                                                  ? 'Din-in' ' ${e.orderId}'
+                                                                  : 'TakeAway' ' ${e.orderId}',
                                                               //'${e.type!.contains('TakeAway') ? '${e.type!.split('').first}T' : e.type!.split('').first} ${e.orderId}',
                                                               style: const TextStyle(
                                                                   fontSize: 33,
@@ -174,8 +176,11 @@ class _FHallState extends State<FHall> {
                                                         //e.type!.contains('TakeAway') ? Icon(Icons.fastfood, color: Color.fromRGBO(14, 35, 46, 1),) : Icon(Icons.baby_changing_station),
                                                         Center(
                                                           child: Text(
-                                                            'TakeAway'' ${e.orderId}',
-                                                            //'${e.type!.contains('TakeAway') ? '${e.type!.split('').first}T' : e.type!.split('').first} ${e.orderId}',
+                                                            e.type == 'Floor'
+                                                            ? 'Din-in' ' ${e.orderId}'
+                                                            : 'TakeAway' ' ${e.orderId}',
+                                                            /*'Din in'' ${e.orderId}',*/
+                                                           //'${e.type!.contains('Floor') ? '${e.type!.split('Din-in')}' : e.type!.split('TakeAway')} ${e.orderId}',
                                                             style: const TextStyle(
                                                               fontSize: 33,
                                                               fontWeight: FontWeight
@@ -226,190 +231,5 @@ class _FHallState extends State<FHall> {
       ],
     ),
         ));
-  }
-  Widget Preparing() {
-    return Padding(
-      padding: const EdgeInsets.only(right: 60),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width*0.25, height: 500,
-        child: StreamBuilder<UnmodifiableListView<Orders>>(
-              stream: bLoC.OrdersStream,
-              builder: (context, snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 25,),
-                  child: ConditionalBuilder(
-                      condition: snapshot.data != null,
-                      builder: (context) => Column(
-                            children: [
-                              SizedBox(
-                                width: 280, height: 70,
-                                child: Container(
-                                  padding: const EdgeInsets.all(12),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 40),
-                                    child: Row(
-                                      children: const [
-                                        Center(
-                                            child: Text(
-                                              'Preparing ',
-                                              style: TextStyle(
-                                                fontFamily: 'Cairo_Regular',
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 33,
-                                                color: Color.fromRGBO(14, 35, 46, 1),
-                                              ),
-                                            )),
-                                        Icon(Icons.baby_changing_station,
-                                          color: Color.fromRGBO(14, 35, 46, 1),)
-                                      ],
-                                    ),
-                                  ),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color.fromRGBO(249, 183, 0, 1)
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 10),
-                                child: CarouselSlider(
-                                  options: CarouselOptions(height: 380, autoPlay: true,scrollDirection: Axis.vertical,viewportFraction: 0.32,),
-                                  items: snapshot.data!.where((element) => element.status == 'Preparing').map((e) =>
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 20, left: 80.0, right: 80),
-                                        child: SizedBox(
-                                          width: 232, height: 70,
-                                          child: Container(
-                                            padding: const EdgeInsets.all(12),
-                                            child:
-                                            //e.type!.split('').first}T' : e.type!.split('').first'${e.type!.split('').first}T' : e.type!.split('').first}
-                                            //e.type!.contains('TakeAway') ? Icon(Icons.fastfood, color: Color.fromRGBO(14, 35, 46, 1),) : Icon(Icons.baby_changing_station),
-                                            Center(
-                                              child: Text(
-                                                'TakeAway'' ${e.orderId}',
-                                                //'${e.type!.contains('TakeAway') ? '${e.type!.split('').first}T' : e.type!.split('').first} ${e.orderId}',
-                                                style: const TextStyle(
-                                                  fontSize: 33,
-                                                  fontWeight: FontWeight
-                                                      .bold,
-                                                  color: Color.fromRGBO(
-                                                      249, 183, 0, 1),
-                                                ),
-                                              ),
-                                            ),
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                              BorderRadius.circular(10),
-                                              color: Color.fromRGBO(
-                                                  14, 35, 46, 1),
-                                            ),
-                                          ),
-                                        ),)).toList(),
-                                ),
-                              ),
-                            ],
-                          ), fallback: (context) => Padding(
-                            padding: const EdgeInsets.only(top: 200.0),
-                            child: Lottie.network('https://assets10.lottiefiles.com/packages/lf20_vkqybeu5/data.json'),
-                          )),
-                );
-              }
-        ),
-      ),
-    );
-  }
-  Widget Finished() {
-    return Padding(
-      padding: const EdgeInsets.only(left: 60),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width*0.25, height: 500,
-        child: StreamBuilder<UnmodifiableListView<Orders>>(
-              stream: bLoC.OrdersStream,
-              builder: (context, snapshot) {
-
-                return Padding(
-                  padding: const EdgeInsets.only(top: 25),
-                  child: ConditionalBuilder(
-                      condition: snapshot.data != null,
-                      builder: (context) =>
-                          Column(
-                            children: [
-                              SizedBox(
-                                width: 280, height: 70,
-                                child: Container(
-                                  padding: const EdgeInsets.all(12),
-                                  child: Row(
-                                    children: const [
-                                      Center(
-                                          child: Text(
-                                            ' Recieve Now ',
-                                            style: TextStyle(
-                                              fontFamily: 'Cairo_Regular',
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 33,
-                                              color: Color.fromRGBO(249, 183, 0, 1)
-
-                                            ),
-                                          )),
-                                      Icon(Icons.fastfood,
-                                        color: Color.fromRGBO(249, 183, 0, 1),),
-                                    ],
-                                  ),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.green,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 25),
-                                child: CarouselSlider(
-                                  options: CarouselOptions(height: 380, autoPlay: true,scrollDirection: Axis.vertical,viewportFraction: 0.32,),
-                                  items: snapshot.data!.where((element) =>
-                                  element.status == 'Finished').map((e) =>
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 20,
-                                            left: 80.0, right: 80),
-                                        child: SizedBox(
-                                          width: 232, height: 70,
-                                          child: Container(
-                                            padding: const EdgeInsets.all(12),
-                                            child:
-                                            //e.type!.split('').first}T' : e.type!.split('').first'${e.type!.split('').first}T' : e.type!.split('').first}
-                                            //e.type!.contains('TakeAway') ? Icon(Icons.fastfood, color: Color.fromRGBO(14, 35, 46, 1),) : Icon(Icons.baby_changing_station),
-                                            Center(
-                                              child: Text(
-                                                'TakeAway'' ${e.orderId}',
-                                                //'${e.type!.contains('TakeAway') ? '${e.type!.split('').first}T' : e.type!.split('').first} ${e.orderId}',
-                                                style: const TextStyle(
-                                                  fontSize: 33,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.green
-                                                ),
-                                              ),
-                                            ),
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                              BorderRadius.circular(10),
-                                              color: const Color.fromRGBO(
-                                                  249, 183, 0, 1),
-                                            ),
-                                          ),
-                                        ),
-                                      ),).toList(),
-
-                                ),
-                              ),
-                            ],
-
-                          ), fallback: (context) => Lottie.network('https://assets5.lottiefiles.com/private_files/lf30_fqBsFC.json'),
-                    ),
-                );
-              }
-          ),
-      ),
-    );
   }
 }
